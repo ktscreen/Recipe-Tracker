07a001cbd4a751f107267d84eb6efa1073e82bb2
ktscreen <83034562+ktscreen@users.noreply.github.com>
2023-05-04 11:45:39 -0400

    Add files via upload

---
+++ src/Activity.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Activity.css";
+
+function Activity({time, activity}) {
+  return (
+    {time, activity}
+  );
+}
+
+export default Activity;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header({imageSrc, name, birthday}) {
+  return (
+    <header>
+      <img src ={imageSrc}></img>
+      <div>{name}</div>
+      <div>{birthday}</div>
+    </header>
+  );
+}
+
+export default Header;
